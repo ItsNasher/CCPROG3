c7df803ba6ea57bd260adc7c44044902ab5f99a6
ItsNasher <165283236+ItsNasher@users.noreply.github.com>
2024-05-19 14:52:49 +0800

    Delete BankAccount directory

--- BankAccount/src/MainDriver.java
+++
@@ -1,9 +0,0 @@
-public class MainDriver {
-    public static void main(String[] args){
-        Main account = new Main("12306096", "Ethan Dela Cruz", 500.0);
-
-        //System.out.println(account.inquire());
-        //System.out.println("Adding 200: " + account.credit(100.0));
-        //System.out.println("Removing 501: " + account.debit(501.0));
-    }
-}
